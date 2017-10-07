92533ae2dafce35b504988a29da483dbf607ba52
Kyung Ho Chun <kho1995@gmail.com>
2017-10-07 23:35:48 +1300

    phos's page update and favicon

---
+++ src/components/dan/Dan.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="dan">
+		<img src="../../assets/kawaii.gif" id="logo_img"/>
+		<h2>{{message}}</h2>
+		<japScroll></japScroll>
+		<div style="margin-top:40px;">
+			<iframe width="50%" height="200" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/357876454&amp;color=%23ff5500&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>
+		</div>
+	</div>
+</template>
+
+<script>
+import japScroll from './japScroll'
+
+const store = {
+	state: {
+		message: 'Feelings By Dan',
+	}
+};
+
+export default {
+  name: 'dan',
+	components: {
+		japScroll,
+	},
+  data() {
+    return {
+			message: store.state.message,
+    };
+  },
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.dan {
+	text-align: center;
+}
+
+h2 {
+  font-weight: normal;
+	margin: 30;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/dan/japScroll.vue
@@ -0,0 +1,13 @@
+<template>
+	<marquee behavior="scroll" direction="left">
+		あの交差点で みんながもしスキップをして もしあの街の真ん中で 手をつないで空を見上げたら もしもあの街のどこかで チャンスがつかみたいのなら まだ泣くのには早いよね ただ前に進むしかないわいやいや 出して しまえばいいの ぜんぜん しないの つまらないでしょ ヘッドフォンかけて リズムに乗せて WAYWAY 空けて あたしの道を 進む 色々なこと どんどん キテる? あなたのキモチ POIPOI 捨てる悪い子はだれ?	そうそう いいコ ああ You Make Me Happy Every Day Every Time is メリーゴーランド 乗りたいの Every Day Every Time is たぶん そんなんじゃ ダメでしょ 出して しまえばいいの ぜんぜん しないの つまらないでしょ ヘッドフォンかけて リズムに乗せて WAYWAY 空けて あたしの道を うぇいうぇいうぇい うぇいうぇい うぇいうぇい うぇいうぇいうぇい うぇいうぇい うぇいうぇいうぇい うぇいうぇい うぇいうぇい うぇいうぇいうぇい うぇいうぇい 
+	</marquee> 
+</template>
+
+<script>
+export default {
+	name: 'japScroll',
+}
+
+</script>
+<style></style>
